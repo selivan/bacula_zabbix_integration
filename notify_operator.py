b7ba41fe68a5d9b32bec5302fa6a6b4aad338863
Valen Tau <wavilen@gmail.com>
2013-11-22 14:17:20 +0400

    add logging module

--- notify_operator.py
+++ notify_operator.py
@@ -2,50 +2,45 @@
 '''
 Simple script to send Bacula reports to Zabbix
 Made by Pavel Selivanov <selivan5@yandex.ru>
+Forked by Valen Tau <wavilen@gmail.com>
 Should be used in Bacula-dir config instead of mail command:
 mail = <> = all, !skipped
 mailcommand = "/etc/zabbix/bacula_notify.py %r %c"
 operatorcommand = "/etc/zabbix/bacula_notify_operator.py %r <bacula-sd server name>"
 Hostnames in Zabbix and Bacula must correspond
 '''
-
-import sys, re
+import sys
+import re
 import datetime
-
 # Logging
-now=datetime.datetime.today()
-#log=open('/dev/stdout', 'a')
-log=open('/var/log/bacula/notify_log', 'a')
-log.write('--------------------------------------\n')
-log.write(now.isoformat() + '\n')
-log.write('sys.argv: ')
-for str in sys.argv:
-	log.write(str + ' ')
-	log.write('\n')
+import logging
+logging.basicConfig(
+    format=u'%(levelname)-8s [%(asctime)s] %(message)s',
+    level=logging.DEBUG,
+    filename=u'/var/log/bacula/{0}.log'.format(os.path.basename(__file__))
+)
+logging.info('sys.argv: ')
+for _str in sys.argv:
+    logging.info(_str)
 
 # Handle incorrect call
-if sys.version_info>=(3,):
-    print "Need python version 2 to run. Tested with python 2.6"
+if sys.version_info >= (3,):
+    logging.warn("Need python version 2 to run. Tested with python 2.6")
     quit(1)
 
 if len(sys.argv) < 3:
-    print "Usage:"
-    print sys.argv[0], " ZABBIX_SERVER HOSTNAME CUSTOM_MESSAGE"
+    logging.warn(
+        "Usage: {0} ZABBIX_SERVER HOSTNAME CUSTOM_MESSAGE".format(sys.argv[0]))
     quit(5)
 
 # Settings
 from conf import conf
 
-# Get values from arguments
-zabbix_sender = conf['zabbix_sender']
-zabbix_server = sys.argv[1]
-hostname = conf['hostname']
-#custom_message = sys.argv[3]
-custom_message = sys.stdin.read()
-
-command = "%(zabbix_sender)s -z %(zabbix_server)s -s %(hostname)s -k 'bacula.custommessage' -o '%(custom_message)s'" % vars()
-log.write(command + '\n')
-subprocess.call (command, shell=True)
-
-log.close()
-
+command = "{0} -z {1} -s {2} -k 'bacula.custommessage' -o '{3}'".format(
+    conf['zabbix_sender'],
+    sys.argv[1],
+    conf['hostname'],
+    sys.stdin.read()
+)
+logging.info(command)
+subprocess.call(command, shell=True)
